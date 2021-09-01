3f386270a774ad09893f868f1d8fcb605c0a4655
jeevabharathy66@gmail.com <jeevabharathy66@gmail.com>
2021-09-01 21:39:30 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,48 @@
+import path from "path";
+import fs from "fs";
+
+import React from "react";
+import express from "express";
+import ReactDOMServer from "react-dom/server";
+
+import App from "../src/App";
+
+const PORT = process.env.PORT || 3006;
+const app = express();
+
+app.get("/", (req, res) => {
+  const app = ReactDOMServer.renderToString(<App />);
+  console.log("app", app);
+
+  const indexFile = path.resolve("./build/index.html");
+  fs.readFile(indexFile, "utf8", (err, data) => {
+    if (err) {
+      console.error("Something went wrong:", err);
+      return res.status(500).send("Oops, better luck next time!");
+    }
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+    );
+  });
+});
+
+app.use(express.static("./build"));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+/*
+ ReactDOMServer.renderToString(<App />);
+
+ 
+Render a React element to its initial HTML. This should only be used on the server. 
+
+React will return an HTML string. You can use this method to generate HTML on the server and send the markup 
+down on the initial request for faster page loads and to allow search engines to crawl your pages for SEO purposes.
+
+If you call ReactDOM.hydrate() on a node that already has this server-rendered markup, 
+
+React will preserve it and only attach event handlers, allowing you to have a very performant first-load experience.
+*/
